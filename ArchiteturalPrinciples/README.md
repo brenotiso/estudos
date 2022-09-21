623f08b24722307268912eeb19e8882c61aa16ef
Breno Tiso <brenotiso@hotmail.com>
2022-09-20 21:29:18 -0300

    api para teste de cache

---
+++ ArchiteturalPrinciples/README.md
@@ -0,0 +1,18 @@
+## Policy vs Detail
+   Como separar regras de implementações.
+   https://khalilstemmler.com/articles/enterprise-typescript-nodejs/clean-nodejs-architecture
+
+## Coupling & Cohesion
+   Interdependência entre modulos/funcionaldiades.
+   https://www.geeksforgeeks.org/software-engineering-coupling-and-cohesion/
+   https://www.javatpoint.com/software-engineering-coupling-and-cohesion
+
+## Component principles  
+   Como organizar os compeonentes e suas dependências.
+   https://khalilstemmler.com/wiki/stable-abstraction-principle/
+   https://khalilstemmler.com/wiki/stable-dependency-principle/
+
+## Boundaries
+   Como identificar os subdomínios de uma funcionalidade.
+   https://khalilstemmler.com/articles/enterprise-typescript-nodejs/application-layer-use-cases/
+
---
+++ TodoApi/TodoApi/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Database;
+using TodoApi.Extensions;
+using TodoApi.Middlewares;
+using TodoApi.Repositories;
+using TodoApi.Repositories.Interfaces;
+using TodoApi.Services;
+using TodoApi.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContextPool<TodoContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("TodoConnection"));
+});
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+
+builder.Services.AddScoped<ITodoService, TodoService>();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioning(setupAction =>
+{
+    setupAction.AssumeDefaultVersionWhenUnspecified = true;
+    setupAction.DefaultApiVersion = new ApiVersion(1, 0);
+    setupAction.ReportApiVersions = true;
+    setupAction.ApiVersionReader = ApiVersionReader.Combine(
+        new MediaTypeApiVersionReader("ver"));
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.MigrateDatabase();
+
+app.Run();
